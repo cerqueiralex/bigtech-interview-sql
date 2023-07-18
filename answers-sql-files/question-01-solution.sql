bce8ef808512348b96aac54b82d841fd55c672bc
Alexandre Cerqueira <36899045+cerqueiralex@users.noreply.github.com>
2023-07-18 13:51:11 -0300

    Delete answers-sql-files directory

--- answers-sql-files/question-01-solution.sql
+++
@@ -1,7 +0,0 @@
--- Assume we have a table of employee information, which includes salary information. 
--- Write a query to find the names and salaries of the top 5 highest paid employees, in descending order.
-
-SELECT name, salary
-FROM employee
-ORDER BY salary DESC
-LIMIT 5;
